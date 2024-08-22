7a2d0a6055b8b176fbd4c5b01b69c3190d959209
Fadi Ajami <fadi.ajami@montyholding.com>
2024-08-22 15:13:39 +0300

    Testing SSO 2

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -1,28 +1,3 @@
-<!-- <template>
-  <div></div>
-</template>
-
-<script>
-export default {
-  name: "App",
-  async mounted() {
-    const data = await window.ghl.getUserData();
-    console.log("user-details", data);
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style> -->
-
 <template>
   <div>
     <h1>MontyPay Configuration</h1>
@@ -40,37 +15,38 @@ export default {
       </fieldset>
       <button type="submit">Save</button>
     </form>
-    {{ JSON.stringify(user, null, 4) }}
+    <p>
+      {{ JSON.stringify(user, null, 4) }}
+    </p>
   </div>
 </template>
 
 <script>
 import { ref, onMounted } from "vue";
 
-// export default {
-//   name: "App",
-//   setup() {
-//     const user = ref({}); // Define user as a reactive reference
+const user = ref({}); // Define user as a reactive reference
+export default {
+  name: "App",
+  setup() {
+    onMounted(async () => {
+      const data = await window.ghl.getUserData();
+      console.log("user-details", data);
+      user.value = data; // Update the user reference with the fetched data
+    });
 
-//     onMounted(async () => {
-//       const data = await window.ghl.getUserData();
-//       console.log("user-details", data);
-//       user.value = data; // Update the user reference with the fetched data
-//     });
+    return { user }; // Return the reactive reference for use in the template
+  },
+};
 
-//     return { user }; // Return the reactive reference for use in the template
-//   },
-// };
+// const user = ref({});
+// async function getUserData() {
+//   const data = await window.ghl.getUserData();
+//   user.value = data;
+// }
 
-const user = ref({});
-async function getUserData() {
-  const data = await window.ghl.getUserData();
-  user.value = data;
-}
-
-onMounted(() => {
-  getUserData();
-});
+// onMounted(() => {
+//   getUserData();
+// });
 </script>
 
 <style>
