dd4d4e33fd1272e91e62be8df841fc5bdc7632ad
Fadi Ajami <fadi.ajami@montyholding.com>
2024-08-23 12:10:29 +0300

    Reverted back to vue

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -35,4 +35,13 @@ onMounted(() => {
 });
 </script>
 
-<style></style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
