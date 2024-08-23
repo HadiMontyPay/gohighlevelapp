3c2cdd81e3d754972034018d100fc86ca6c6e1f2
Fadi Ajami <fadi.ajami@montyholding.com>
2024-08-23 12:42:21 +0300

    Added Merchant Infor Fields

--- src/ui/src/App.vue
+++ src/ui/src/App.vue
@@ -1,31 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>MontyPay Configuration</h1>
+    <form>
+      <fieldset>
+        <legend>Merchant Info</legend>
+        <label>
+          Merchant Key:
+          <input type="text" placeholder="Merchant Key" />
+        </label>
+        <label>
+          Merchant Password:
+          <input type="password" placeholder="Merchant Password" />
+        </label>
+      </fieldset>
+      <button type="submit">Save</button>
+    </form>
+    <div>
+      {{ JSON.stringify(user, null, 4) }}
+    </div>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, onMounted } from "vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  async mounted(){
-    
-    const data = await window.ghl.getUserData();
-    console.log("user-details", data)
-  }
+const user = ref({});
+async function getUserData() {
+  const data = await window.ghl.getUserData();
+  user.value = data;
 }
+
+onMounted(() => {
+  getUserData();
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
