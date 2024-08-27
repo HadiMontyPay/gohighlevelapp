79c94aaac4eb30d4dec6c3f990bc7c666ab8f66c
Fadi Ajami <fadi.ajami@montyholding.com>
2024-08-27 11:24:23 +0300

    Fetch V2.2

--- src/ui/src/components/Settings.vue
+++ src/ui/src/components/Settings.vue
@@ -78,9 +78,7 @@ async function association() {
     const locationIdData = await locationIdResponse.json();
     console.log(locationIdData.locationId);
 
-    const baseUrl = process.env.GHL_API_DOMAIN;
-
-    const url = `${baseUrl}/payments/custom-provider/provider?locationId=${locationIdData.locationId}`;
+    const url = `https://services.leadconnectorhq.com/payments/custom-provider/provider?locationId=${locationIdData.locationId}`;
 
     const headers = {
       Accept: "application/json",
