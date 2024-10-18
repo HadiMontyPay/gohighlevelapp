6746c935b46caadb4101419cae044b3b8c32d299
Fadi Ajami <fadi.ajami@montyholding.com>
2024-10-18 15:05:55 +0300

    Removed WS

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,6 @@ import sequelize from "./database"; // Adjust path if necessary
 import { GHL } from "./ghl";
 import cors from "cors";
 import CryptoJS from "crypto-js";
-import { WebSocketServer } from "ws";
-import http from "http";
 
 const path = __dirname + "/ui/dist/";
 
@@ -17,30 +15,6 @@ dotenv.config();
 const app: Express = express();
 app.use(json({ type: "application/json" }));
 app.use(urlencoded({ extended: true }));
-
-const server = http.createServer(app);
-const wss = new WebSocketServer({ server });
-// WebSocket connection logic
-wss.on("connection", (ws) => {
-  console.log("Client connected via WebSocket");
-
-  // Send data to the connected client
-  ws.send(JSON.stringify({ message: "Hello from WebSocket server!" }));
-
-  // Handle incoming messages from the client
-  ws.on("message", (message: string) => {
-    console.log(`Received message: ${message}`);
-  });
-
-  // Handle WebSocket disconnection
-  ws.on("close", () => {
-    console.log("Client disconnected");
-  });
-
-  ws.on("error", (error) => {
-    console.error("WebSocket error:", error);
-  });
-});
 
 // Set up CORS options if needed
 const corsOptions = {
@@ -395,7 +369,7 @@ app.post("/notifications", (req: Request, res: Response) => {
   // const notification: NotificationPayload = req.body;
   const notification = req.body;
 
-  // console.log(document.referrer);
+  console.log(document.referrer);
 
   // Log the notification or process it
   console.log("Received notification:", notification);
