b1a520bc7f2a71137b27bd0b9c6d01abff3b9ac9
Fadi Ajami <fadi.ajami@montyholding.com>
2025-01-10 15:36:00 +0200

    Returned the url_target

--- src/index.ts
+++ src/index.ts
@@ -364,7 +364,7 @@ app.post("/getPaymentRedirectURL", async (req: Request, res: Response) => {
     operation,
     cancel_url,
     success_url,
-    // url_target,
+    url_target,
     order,
     customer,
   } = req.body;
@@ -405,7 +405,7 @@ app.post("/getPaymentRedirectURL", async (req: Request, res: Response) => {
       cancel_url: cancel_url,
       success_url: success_url,
       hash: `${result}`,
-      // url_target: url_target,
+      url_target: url_target,
       order: {
         description: order.description,
         number: order.number,
