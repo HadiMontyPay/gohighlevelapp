63dc882d99d90547dd46a1bce1a47615131c85e4
MontyPayDev <hadi.hassoun@montypay.com>
2025-06-19 11:56:44 +0300

    Added Cookies storage in the Iframe

--- src/ui/src/components/PaymentPage.vue
+++ src/ui/src/components/PaymentPage.vue
@@ -7,7 +7,7 @@
     <iframe
       :src="iframeSrc"
       v-if="ll === false && errorState === false"
-      allow="payment *; fullscreen"
+      allow="payment *; cookies; fullscreen"
     ></iframe>
     <div id="lll" v-if="ll === true">
       <div class="loader"></div>
